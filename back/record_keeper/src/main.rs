195da5826d82ec037915e27e4d2c5540c41425c0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-05-12 14:35:35 -0700

    wired up the models and the endpoints

--- back/record_keeper/src/main.rs
+++ back/record_keeper/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::FileServer;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+
+mod endpoints;
+mod models;
+
+use endpoints::health;
+
+#[launch]
+fn rocket() -> _ {
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
+    rocket::build()
+        .attach(cors)
+        .mount("/", FileServer::from("front"))
+        .mount("/api", routes![health::health_check])
 }
