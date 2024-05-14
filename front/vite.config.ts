0deaaf13f3d10bea3aa04837033166b2c43bf04b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-05-14 23:35:44 +0200

    was just missing root as the directory to serve

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
-  publicDir: 'public',
+  root: 'public',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
