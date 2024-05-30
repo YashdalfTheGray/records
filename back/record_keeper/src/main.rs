dbf72e7ba6409e9a80470a9af03ccd4022835ad7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-05-30 15:52:09 -0700

    implment app config to try and solve the lifetime problem

--- back/record_keeper/src/main.rs
+++ back/record_keeper/src/main.rs
@@ -3,14 +3,27 @@ extern crate rocket;
 
 use rocket::fs::FileServer;
 use rocket_cors::{AllowedOrigins, CorsOptions};
+use std::env;
 
 mod endpoints;
 mod models;
 
-use endpoints::health;
+use endpoints::{dev_proxy, health};
+use models::app_config::AppConfig;
+
+fn is_dev() -> bool {
+    env::var("ROCKET_ENV")
+        .unwrap_or_else(|_| "development".into())
+        .to_lowercase()
+        == "development".to_lowercase()
+}
 
 #[launch]
 fn rocket() -> _ {
+    let vite_dev_server =
+        env::var("vite_dev_server").unwrap_or_else(|_| "http://localhost:3000".into());
+    let appConfig = AppConfig { vite_dev_server };
+
     let cors = CorsOptions {
         allowed_origins: AllowedOrigins::all(),
         allow_credentials: true,
@@ -19,8 +32,18 @@ fn rocket() -> _ {
     .to_cors()
     .unwrap();
 
-    rocket::build()
-        .attach(cors)
-        .mount("/", FileServer::from("front"))
-        .mount("/api", routes![health::health_check])
+    let mut rocket = rocket::build().attach(cors).manage(appConfig);
+
+    rocket = rocket.mount("/api", routes![health::health_check]);
+
+    if is_dev() {
+        rocket = rocket.mount("/", routes![dev_proxy::vite_proxy]);
+    } else {
+        rocket = rocket.mount(
+            "/",
+            FileServer::from(concat!(env!("CARGO_MANIFEST_DIR"), "/dist")),
+        );
+    }
+
+    rocket
 }
