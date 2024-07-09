d163880aaaa166dfcfe369a2e1b300f49f0636c4
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-07-08 23:10:55 -0700

    added an endpoint that returns a particular album back to the frontend

--- back/record_keeper/src/endpoints/get_album.rs
+++ back/record_keeper/src/endpoints/get_album.rs
@@ -1,16 +1,33 @@
-use crate::utils;
+use std::{error::Error, fs::File, io::BufReader, path::Path};
+
+use rocket::{http::Status, response::status, serde::json::Json, State};
+
+use crate::{
+    models::{album_details, app_config},
+    utils,
+};
 
 #[get("/album?<album>&<artist>")]
 pub fn get_album_details(
-    config: &State<AppConfig>,
+    config: &State<app_config::AppConfig>,
     album: &str,
     artist: &str,
-) -> Result<Json<AlbumDetails>, status::Custom<String>> {
-    match utils::spotify_creds::access_token(config.spotify_auth_endpoint) {
+) -> Result<Json<album_details::Album>, status::Custom<String>> {
+    match utils::spotify_creds::access_token(config.spotify_auth_endpoint.to_string()) {
         Ok(access_token) => {
-            let album_Details = AlbumDetails {};
-            Ok(Json(album_Details))
+            let file_path = "resources/album.json";
+            match read_album_from_file(file_path) {
+                Ok(album_details) => Ok(Json(album_details)),
+                Err(e) => Err(status::Custom(Status::InternalServerError, "".to_string())),
+            }
         }
-        Err(e) => return Err(status::Custom(Status::ServerError, "")),
+        Err(e) => Err(status::Custom(Status::InternalServerError, "".to_string())),
     }
+}
+
+fn read_album_from_file<P: AsRef<Path>>(path: P) -> Result<album_details::Album, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let album = serde_json::from_reader(reader)?;
+    Ok(album)
 }
