263c0ceb778520af76962315375cdb6e35986efb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-07-09 22:14:05 -0700

    added a proxy that is supposed to pick up the host ip address and act accordingly

--- back/record_keeper/src/endpoints/dev_proxy.rs
+++ back/record_keeper/src/endpoints/dev_proxy.rs
@@ -1,11 +1,12 @@
-use rocket::http::uri::Absolute;
 use rocket::response::Redirect;
 use rocket::State;
 
-use crate::models::app_config::AppConfig;
+use crate::models::{app_config::AppConfig, host_details::HostIpAddress};
 
 #[get("/<_..>", rank = 2)]
-pub fn vite_proxy(config: &State<AppConfig>) -> Redirect {
-    let absolute_uri = Absolute::parse(&config.vite_dev_server).unwrap();
-    Redirect::to(absolute_uri)
+pub fn vite_proxy(config: &State<AppConfig>, host: HostIpAddress) -> Redirect {
+    print!("{}", host);
+    let host_and_port = format!("http://{}:{}/", host, config.vite_dev_server_port);
+
+    Redirect::to(host_and_port)
 }
