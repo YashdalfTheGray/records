96ee5b45a046e92f24ff897bd98b924c64ceb185
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-07-14 19:58:57 -0700

    pulled all the host ip address wiring shit out so that we can get something working'

--- back/record_keeper/src/endpoints/dev_proxy.rs
+++ back/record_keeper/src/endpoints/dev_proxy.rs
@@ -1,12 +1,11 @@
 use rocket::response::Redirect;
 use rocket::State;
 
-use crate::models::{app_config::AppConfig, host_details::HostIpAddress};
+use crate::models::app_config::AppConfig;
 
 #[get("/<_..>", rank = 2)]
-pub fn vite_proxy(config: &State<AppConfig>, host: HostIpAddress) -> Redirect {
-    print!("{}", host);
-    let host_and_port = format!("http://{}:{}/", host, config.vite_dev_server_port);
+pub fn vite_proxy(config: &State<AppConfig>) -> Redirect {
+    let host_and_port = format!("http://localhost:{}/", config.vite_dev_server_port);
 
     Redirect::to(host_and_port)
 }
