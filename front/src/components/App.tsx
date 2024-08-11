5a854fc99a861b7195e7c573cc8543550401f631
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-08-11 10:57:03 -0700

    added the cssbaseline component into the app

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,9 +1,11 @@
 import { h } from 'preact';
 import { Hello } from './Hello';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const App = () => {
   return (
     <div>
+      <CssBaseline />
       <h1>Welcome to Preact with TypeScript!</h1>
       <Hello name="World" />
     </div>
