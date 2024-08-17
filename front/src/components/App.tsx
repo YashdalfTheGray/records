6163a4e02c6b1c9e23fd59b00cfed562ad66b2e5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-08-17 11:22:36 -0700

    added all those components into the main app

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,13 +1,23 @@
 import { h } from 'preact';
-import { Hello } from './Hello';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+
+import { Hello } from './Hello';
+import NavBar from './NavBar';
+
+const appTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export const App = () => {
   return (
-    <div>
-      <CssBaseline />
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline enableColorScheme />
+      <NavBar />
       <h1>Welcome to Preact with TypeScript!</h1>
       <Hello name="World" />
-    </div>
+    </ThemeProvider>
   );
 };
