8773efe815502c03768cd96595a1e81423fc8c32
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-08-26 06:28:32 -0700

    clear out the app root element before rendering preact into it

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,5 +5,8 @@ import { App } from './components/App';
 const root = document.getElementById('root');
 
 if (root) {
+  // Because render appends (or really, prepends)
+  // we clear out the app root and then render.
+  root.innerHTML = '';
   render(<App />, root);
 }
