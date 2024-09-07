a3deb5b6ef6b20ff815732160b1464d6d57c86e2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-09-07 11:41:27 -0700

    set the default light/dark mode to system to start out with

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,17 +1,26 @@
 import { h } from 'preact';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {
+  ThemeProvider,
+  createTheme,
+  useColorScheme,
+} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import { Hello } from './Hello';
 import NavBar from './NavBar';
 
 const appTheme = createTheme({
-  palette: {
-    mode: 'dark',
+  colorSchemes: {
+    dark: true,
   },
 });
 
 export const App = () => {
+  const { mode, setMode } = useColorScheme();
+  if (!mode) {
+    setMode('system');
+  }
+
   return (
     <ThemeProvider theme={appTheme}>
       <CssBaseline enableColorScheme />
