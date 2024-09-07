d7b4aad8d6e816fc4805a052b7d8a497799e9fd3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-09-07 11:41:03 -0700

    made the theme mode switch so much simpler thanks to mui 6

--- front/src/components/ThemeModeSwitch.tsx
+++ front/src/components/ThemeModeSwitch.tsx
@@ -1,43 +1,19 @@
-import { ThemeProvider } from '@emotion/react';
-import { createTheme } from '@mui/material';
+import { useColorScheme } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import { useState, useMemo } from 'preact/hooks';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 export default function ThemeModeSwitch() {
-  const [mode, setMode] = useState<'light' | 'dark'>('light');
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    []
-  );
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
+  const { mode, setMode } = useColorScheme();
 
   return (
-    <Tooltip title={`${theme.palette.mode} mode`}>
+    <Tooltip title={`${mode} mode`}>
       <IconButton
-        aria-label={`${theme.palette.mode} mode`}
-        onClick={colorMode.toggleColorMode}
+        aria-label={`${mode} mode`}
+        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
       >
-        {theme.palette.mode === 'dark' ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
+        {mode === 'light' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
     </Tooltip>
   );
