3399231004451bcfb4efcebbdcf2178215b0e014
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2024-11-11 08:18:20 -0800

    changed the outdir path relative to public/index.html which is what vite considers the project root

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,14 +6,8 @@ import preact from '@preact/preset-vite';
 export default defineConfig({
   plugins: [preact({})],
   root: 'public',
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000',
-    },
-  },
-  build: {
-    outDir: '../back/record_keeper/dist',
-  },
+  server: { proxy: { '/api': 'http://localhost:8000' } },
+  build: { outDir: '../../back/record_keeper/dist' },
   resolve: {
     alias: {
       'react-dom/test-utils': 'preact/test-utils',
