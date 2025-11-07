a9b6c700620b16874f83ea2021691e50a3b82819
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2025-11-06 19:42:40 -0800

    changed the mounting point for the apis since we mount them differently now

--- back/endpoints/health.rb
+++ back/endpoints/health.rb
@@ -2,7 +2,7 @@ require_relative 'base'
 
 module Endpoints
   class Health < Endpoints::Base
-    get '/api/health' do
+    get '/' do
       content_type :json
       status 200
       { status: 'okay' }.to_json
